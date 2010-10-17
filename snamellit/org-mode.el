e0637a97f97ed038933d21dcee98b9fd6504744b
Peter Tillemans <pti@snamellit.com>
2010-10-17 14:48:21 +0200

    Tweking org-mode

--- snamellit/org-mode.el
+++ snamellit/org-mode.el
@@ -1,6 +1,15 @@
+
 (add-to-list 'load-path "~/.emacs.d/vendor/org-mode/lisp" "~/.emacs.d/vendor/org-mode/contrib/lisp")
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-font-lock-mode 1)
+
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/org")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/MobileOrg")
+
 (require 'org-install)
