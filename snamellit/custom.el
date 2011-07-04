fffd86f6fbbc3ac7bf47238969ff5cdfb48ce129
Peter Tillemans <pti@melexis.com>
2011-07-05 01:23:11 +0200

    Removed custom override of the selected theme.

--- snamellit/custom.el
+++ snamellit/custom.el
@@ -6,7 +6,6 @@
  '(aquamacs-additional-fontsets nil t)
  '(aquamacs-customization-version-id 210 t)
  '(aquamacs-tool-bar-user-customization nil t)
- '(default-frame-alist (quote ((tool-bar-lines . 0) (fringe) (right-fringe) (left-fringe . 1) (internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (menu-bar-lines . 1) (background-color . "black") (background-mode . dark) (border-color . "black") (cursor-color . "#A7A7A7") (foreground-color . "#F8F8F8") (mouse-color . "sienna1"))))
  '(emulate-mac-french-keyboard-mode t)
  '(flyspell-use-meta-tab nil)
  '(ns-tool-bar-display-mode nil t)
