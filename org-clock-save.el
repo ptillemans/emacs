440edb36ffab914db67199dfb3ab229b64c32cfa
Peter Tillemans <pti@snamellit.com>
2011-07-11 13:37:37 +0200

    Some modifications to the vendor packages.

--- org-clock-save.el
+++ org-clock-save.el
@@ -1,1 +1,2 @@
-;; org-persist.el - pti-air.local at <2011-07-05 Tue 12:30>
+;; org-persist.el - ptimac.local at <2011-07-11 Mon 13:27>
+(setq stored-clock-history '(("/Users/pti/org/emacs.org" . 22428) ("/Users/pti/org/melexis/melexis.org" . 10729)))
