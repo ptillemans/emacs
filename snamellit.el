26c5a061b43d8f57359a07fb805a9c7d177d431e
Peter Tillemans <pti@snamellit.com>
2011-12-29 20:46:35 +0100

    Removed real auto-save as it removed characters under certain circumstances. idem with magit as the upstream had dissappeared. Fixed gist support from emacs

--- snamellit.el
+++ snamellit.el
@@ -22,10 +22,8 @@
 (load "snamellit/coffee")
 (load "snamellit/yasnippet-bundle")
 (load "snamellit/org-mode")
-;;(load "snamellit/slime")
 (load "snamellit/groovy")
 (load "snamellit/gnus")
-(load "snamellit/real-auto-save")
 
 (when (file-exists-p "snamellit/private")
   (load "snamellit/private"))
@@ -52,7 +50,3 @@
 (setq custom-file "~/.emacs.d/snamellit/custom.el")
 (when (file-exists-p custom-file) (load custom-file))
 
-(require 'real-auto-save)
-(add-hook 'text-mode-hook 'turn-on-real-auto-save)
-(add-hook 'org-mode-hook 'turn-on-real-auto-save)
-(add-hook 'clojure-mode-hook 'turn-on-real-auto-save)
