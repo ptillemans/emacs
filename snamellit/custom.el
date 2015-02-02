67a173f95e1d05164db4db4b6f70f2e5ce9519b5
Peter Tillemans <pti@snamellit.com>
2015-02-02 12:37:55 +0100

    google cal integration and org narrowing
    - Notes on integrating with Google Calendar
    - Notes on narrowing nodes is org mode
    - change of theme to have titles line up in column mode

--- snamellit/custom.el
+++ snamellit/custom.el
@@ -3,75 +3,141 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector [solarized-bg red green yellow blue magenta cyan solarized-fg])
+ '(ansi-color-names-vector
+   [solarized-bg red green yellow blue magenta cyan solarized-fg])
  '(compilation-message-face (quote default))
  '(cua-global-mark-cursor-color "#2aa198")
  '(cua-normal-cursor-color "#839496")
  '(cua-overwrite-cursor-color "#b58900")
  '(cua-read-only-cursor-color "#859900")
- '(custom-safe-themes (quote ("3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
+ '(custom-enabled-themes (quote (sanityinc-solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "3a727bdc09a7a141e58925258b6e873c65ccf393b2240c51553098ca93957723" "6a37be365d1d95fad2f4d185e51928c789ef7a4ccf17e7ca13ad63a8bf5b922f" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "d2622a2a2966905a5237b54f35996ca6fda2f79a9253d44793cfe31079e3c92b" "501caa208affa1145ccbb4b74b6cd66c3091e41c5bb66c677feda9def5eab19c" default)))
  '(fci-rule-color "#073642")
  '(highlight-changes-colors (quote ("#d33682" "#6c71c4")))
- '(highlight-symbol-colors (--map (solarized-color-blend it "#002b36" 0.25) (quote ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
+ '(highlight-symbol-colors
+   (--map
+    (solarized-color-blend it "#002b36" 0.25)
+    (quote
+     ("#b58900" "#2aa198" "#dc322f" "#6c71c4" "#859900" "#cb4b16" "#268bd2"))))
  '(highlight-symbol-foreground-color "#93a1a1")
- '(highlight-tail-colors (quote (("#073642" . 0) ("#546E00" . 20) ("#00736F" . 30) ("#00629D" . 50) ("#7B6000" . 60) ("#8B2C02" . 70) ("#93115C" . 85) ("#073642" . 100))))
+ '(highlight-tail-colors
+   (quote
+    (("#073642" . 0)
+     ("#546E00" . 20)
+     ("#00736F" . 30)
+     ("#00629D" . 50)
+     ("#7B6000" . 60)
+     ("#8B2C02" . 70)
+     ("#93115C" . 85)
+     ("#073642" . 100))))
  '(magit-diff-use-overlays nil)
  '(magit-use-overlays nil)
  '(multi-term-scroll-show-maximum-output t)
  '(org-agenda-files
    (quote
-    ("/Users/pti/org/flagged.org" "~/org/melexis/projects/pos/point-of-sales.org" "~/org/snamellit/snm_general.org" "~/org/notes.org" "~/org/melexis/management/brecht.org" "~/org/melexis/melexis.org" "/Users/pti/org/google_calendar.org" "/Users/pti/org/diary.org" "/Users/pti/org/index.org" "/Users/pti/org/personal.org" "/Users/pti/org/refile.org" "/Users/pti/org/todo.org")))
+    ("/Users/pti/org/personal.org" "~/org/melexis/projects/pos/point-of-sales.org" "~/org/melexis/edi-program.org" "/Users/pti/org/flagged.org" "~/org/snamellit/snm_general.org" "~/org/notes.org" "~/org/melexis/management/brecht.org" "~/org/melexis/melexis.org" "/Users/pti/org/google_calendar.org" "/Users/pti/org/diary.org" "/Users/pti/org/index.org" "/Users/pti/org/refile.org" "/Users/pti/org/todo.org")))
  '(org-jira-serv-alist
    (quote
     ((melexis password wqsXSbjSthVqr78Y username pti url\  https://jira\.melexis\.com/jira))))
  '(org-return-follows-link t)
- '(org-structure-template-alist (quote (("S" "#+BEGIN_SRC emacs-lisp
+ '(org-structure-template-alist
+   (quote
+    (("S" "#+BEGIN_SRC emacs-lisp
 ?
 #+END_SRC" "<src lang=\"?\">
 
-</src>") ("s" "#+BEGIN_SRC ?
+</src>")
+     ("s" "#+BEGIN_SRC ?
 
 #+END_SRC" "<src lang=\"?\">
 
-</src>") ("e" "#+BEGIN_EXAMPLE
+</src>")
+     ("e" "#+BEGIN_EXAMPLE
 ?
 #+END_EXAMPLE" "<example>
 ?
-</example>") ("q" "#+BEGIN_QUOTE
+</example>")
+     ("q" "#+BEGIN_QUOTE
 ?
 #+END_QUOTE" "<quote>
 ?
-</quote>") ("v" "#+BEGIN_VERSE
+</quote>")
+     ("v" "#+BEGIN_VERSE
 ?
 #+END_VERSE" "<verse>
 ?
-</verse>") ("V" "#+BEGIN_VERBATIM
+</verse>")
+     ("V" "#+BEGIN_VERBATIM
 ?
 #+END_VERBATIM" "<verbatim>
 ?
-</verbatim>") ("c" "#+BEGIN_CENTER
+</verbatim>")
+     ("c" "#+BEGIN_CENTER
 ?
 #+END_CENTER" "<center>
 ?
-</center>") ("l" "#+BEGIN_LaTeX
+</center>")
+     ("l" "#+BEGIN_LaTeX
 ?
 #+END_LaTeX" "<literal style=\"latex\">
 ?
-</literal>") ("L" "#+LaTeX: " "<literal style=\"latex\">?</literal>") ("h" "#+BEGIN_HTML
+</literal>")
+     ("L" "#+LaTeX: " "<literal style=\"latex\">?</literal>")
+     ("h" "#+BEGIN_HTML
 ?
 #+END_HTML" "<literal style=\"html\">
 ?
-</literal>") ("H" "#+HTML: " "<literal style=\"html\">?</literal>") ("a" "#+BEGIN_ASCII
+</literal>")
+     ("H" "#+HTML: " "<literal style=\"html\">?</literal>")
+     ("a" "#+BEGIN_ASCII
 ?
-#+END_ASCII" "") ("A" "#+ASCII: " "") ("i" "#+INDEX: ?" "#+INDEX: ?") ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">"))))
- '(rcirc-server-alist (quote (("irc.freenode.net" :channels ("#emacs" "#rcirc" "#clojure" "#cucumber" "#leiningen" "#ubuntu-bugs")) ("irc.geekshed.net" \#jupiterbroadcasting nil))) t)
+#+END_ASCII" "")
+     ("A" "#+ASCII: " "")
+     ("i" "#+INDEX: ?" "#+INDEX: ?")
+     ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">"))))
+ '(rcirc-server-alist
+   (quote
+    (("irc.freenode.net" :channels
+      ("#emacs" "#rcirc" "#clojure" "#cucumber" "#leiningen" "#ubuntu-bugs"))
+     ("irc.geekshed.net" \#jupiterbroadcasting nil))) t)
+ '(safe-local-variable-values
+   (quote
+    ((buffer-read-only . 1)
+     (ruby-compilation-executable . "ruby")
+     (ruby-compilation-executable . "ruby1.8")
+     (ruby-compilation-executable . "ruby1.9")
+     (ruby-compilation-executable . "rbx")
+     (ruby-compilation-executable . "jruby"))))
  '(smartrep-mode-line-active-bg (solarized-color-blend "#859900" "#073642" 0.2))
  '(term-default-bg-color "#002b36")
  '(term-default-fg-color "#839496")
  '(vc-annotate-background "#2b2b2b")
- '(vc-annotate-color-map (quote ((20 . "#bc8383") (40 . "#cc9393") (60 . "#dfaf8f") (80 . "#d0bf8f") (100 . "#e0cf9f") (120 . "#f0dfaf") (140 . "#5f7f5f") (160 . "#7f9f7f") (180 . "#8fb28f") (200 . "#9fc59f") (220 . "#afd8af") (240 . "#bfebbf") (260 . "#93e0e3") (280 . "#6ca0a3") (300 . "#7cb8bb") (320 . "#8cd0d3") (340 . "#94bff3") (360 . "#dc8cc3"))))
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#bc8383")
+     (40 . "#cc9393")
+     (60 . "#dfaf8f")
+     (80 . "#d0bf8f")
+     (100 . "#e0cf9f")
+     (120 . "#f0dfaf")
+     (140 . "#5f7f5f")
+     (160 . "#7f9f7f")
+     (180 . "#8fb28f")
+     (200 . "#9fc59f")
+     (220 . "#afd8af")
+     (240 . "#bfebbf")
+     (260 . "#93e0e3")
+     (280 . "#6ca0a3")
+     (300 . "#7cb8bb")
+     (320 . "#8cd0d3")
+     (340 . "#94bff3")
+     (360 . "#dc8cc3"))))
  '(vc-annotate-very-old-color "#dc8cc3")
- '(weechat-color-list (quote (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
+ '(weechat-color-list
+   (quote
+    (unspecified "#002b36" "#073642" "#990A1B" "#dc322f" "#546E00" "#859900" "#7B6000" "#b58900" "#00629D" "#268bd2" "#93115C" "#d33682" "#00736F" "#2aa198" "#839496" "#657b83"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
