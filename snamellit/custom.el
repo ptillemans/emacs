5b58c3d1c38c209318c143b9121dbc8f45e85c08
Peter Tillemans <pti@snamellit.com>
2016-03-17 10:08:13 +0100

    better terminal handling for eshell

--- snamellit/custom.el
+++ snamellit/custom.el
@@ -155,4 +155,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(org-todo ((((background dark) (type graphic)) (:weight bold :foreground "#002b36" :background "#dc322f")) (((background dark) (type tty) (min-colors 256)) (:weight bold :foreground "brightblack" :background "red")) (((background dark) (type tty) (min-colors 16)) (:weight bold :foreground "brightblack" :background "red")) (((background dark) (type tty) (min-colors 8)) (:weight bold :foreground "black" :background "red")) (((background light) (type graphic)) (:weight bold :foreground "#fdf6e3" :background "#dc322f")) (((background light) (type tty) (min-colors 256)) (:weight bold :foreground "brightwhite" :background "red")) (((background light) (type tty) (min-colors 16)) (:weight bold :foreground "brightwhite" :background "red")) (((background light) (type tty) (min-colors 8)) (:weight bold :foreground "white" :background "red")))))
