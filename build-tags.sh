734618b37769a99582c132724a9773338afc20ea
Peter Tillemans <pti@melexis.com>
2016-06-04 13:30:45 +0200

    new snippet and updates in custom.el and
     * disable ido in orgmode
     * fix loading auctex package
     * add auctex-lua
     * add snippet for support manual

--- build-tags.sh
+++ build-tags.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-etags /usr/local/share/emacs/24.3/lisp/*.el /usr/local/share/emacs/site-lisp/*.el snamellit/*.el *.el vendor/*.el vendor/*/*.el elpa/*/*.el
+etags /usr/local/share/emacs/*/lisp/*.el /usr/local/share/emacs/site-lisp/*.el snamellit/*.el *.el vendor/*.el elpa/*/*.el
