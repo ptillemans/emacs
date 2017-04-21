8707d679d67fb2dc53422d41da178a971e595288
Peter Tillemans <pti@melexis.com>
2017-04-21 14:53:08 +0200

    add support for flow, fix plantuml and ditaa location, schemas

--- init.el
+++ init.el
@@ -26,3 +26,4 @@
 (put 'narrow-to-region 'disabled nil)
 (put 'dired-find-alternate-file 'disabled nil)
 (put 'upcase-region 'disabled nil)
+(put 'downcase-region 'disabled nil)
--- snamellit/custom.el
+++ snamellit/custom.el
@@ -227,7 +227,7 @@
   ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">"))))
 '(package-selected-packages
 (quote
- (ghc haskell-cabal toml-mode yaml-mode cargo cargo-mode rust-snippets indent-tools jedi coffee-mode haskell-mode tide solarized-theme rinari org-pomodoro feature-mode ecukes auto-compile use-package helm-projectile magit markdown-mode web-mode company-racer racer flycheck-rust scala-mode rspec-mode ruby-test-mode ruby-additional ein elpy js2-refactor slime go-projectile groovy-mode cider clojure-mode paredit ggtags rainbow-delimiters flycheck multiple-cursors editorconfig command-log-mode which-key helm smart-mode-line mocha expand-region company-tern tern auctex-lua auctex htmlize aggressive-indent aggressive-indent-mode lacarte tidy go-eldoc edit-server skewer-mode virtualenvwrapper scss-mode rvm ruby-refactor ruby-electric ruby-compilation python-django nose go-snippets fill-column-indicator elixir-yasnippets alchemist ace-jump-mode)))
+ (realgud genrnc ghc haskell-cabal toml-mode yaml-mode cargo cargo-mode rust-snippets indent-tools jedi coffee-mode haskell-mode tide solarized-theme rinari org-pomodoro feature-mode ecukes auto-compile use-package helm-projectile magit markdown-mode web-mode company-racer racer flycheck-rust scala-mode rspec-mode ruby-test-mode ruby-additional ein elpy js2-refactor slime go-projectile groovy-mode cider clojure-mode paredit ggtags rainbow-delimiters flycheck multiple-cursors editorconfig command-log-mode which-key helm smart-mode-line mocha expand-region company-tern tern auctex-lua auctex htmlize aggressive-indent aggressive-indent-mode lacarte tidy go-eldoc edit-server skewer-mode virtualenvwrapper scss-mode rvm ruby-refactor ruby-electric ruby-compilation python-django nose go-snippets fill-column-indicator elixir-yasnippets alchemist ace-jump-mode)))
  '(pos-tip-background-color "#073642")
  '(pos-tip-foreground-color "#93a1a1")
 '(rcirc-bright-nicks
@@ -240,6 +240,9 @@
  (("irc.freenode.net" :channels
    ("#emacs" "#rcirc" "#clojure" "#cucumber" "#leiningen" "#ubuntu-bugs"))
   ("irc.geekshed.net" \#jupiterbroadcasting nil))) t)
+'(rng-schema-locating-files
+(quote
+ ("schemas.xml" "/usr/local/Cellar/emacs/25.1/share/emacs/25.1/etc/schema/schemas.xml" "~/.emacs.d/schema/schema.xml")))
 '(safe-local-variable-values
 (quote
  ((python-shell-virtualenv-path . "/Users/pti/playpen/python/gmailpurge")
