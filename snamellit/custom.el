7b2178b051321c8dbc1fe15fe9c5d715568c76d0
Peter Tillemans <pti@melexis.com>
2017-05-05 20:05:42 +0200

    fixed updating rng-schema-locating-files before it exists

--- snamellit/custom.el
+++ snamellit/custom.el
@@ -227,9 +227,9 @@
   ("I" "#+INCLUDE: %file ?" "<include file=%file markup=\"?\">"))))
 '(package-selected-packages
 (quote
- (gist google-contacts googleaccount oauth2 es-mode realgud genrnc ghc haskell-cabal toml-mode yaml-mode cargo cargo-mode rust-snippets indent-tools jedi coffee-mode haskell-mode tide solarized-theme rinari org-pomodoro feature-mode ecukes auto-compile use-package helm-projectile magit markdown-mode web-mode company-racer racer flycheck-rust scala-mode rspec-mode ruby-test-mode ruby-additional ein elpy js2-refactor slime go-projectile groovy-mode cider clojure-mode paredit ggtags rainbow-delimiters flycheck multiple-cursors editorconfig command-log-mode which-key helm smart-mode-line mocha expand-region company-tern tern auctex-lua auctex htmlize aggressive-indent aggressive-indent-mode lacarte tidy go-eldoc edit-server skewer-mode virtualenvwrapper scss-mode rvm ruby-refactor ruby-electric ruby-compilation python-django nose go-snippets fill-column-indicator elixir-yasnippets alchemist ace-jump-mode)))
-'(pos-tip-background-color "#073642")
-'(pos-tip-foreground-color "#93a1a1")
+ (rng-validate-mode rust-mode pyvenv go-mode company yasnippet gist google-contacts googleaccount oauth2 es-mode realgud genrnc ghc haskell-cabal toml-mode yaml-mode cargo cargo-mode rust-snippets indent-tools jedi coffee-mode haskell-mode tide solarized-theme rinari org-pomodoro feature-mode ecukes auto-compile use-package helm-projectile magit markdown-mode web-mode company-racer racer flycheck-rust scala-mode rspec-mode ruby-test-mode ruby-additional ein elpy js2-refactor slime go-projectile groovy-mode cider clojure-mode paredit ggtags rainbow-delimiters flycheck multiple-cursors editorconfig command-log-mode which-key helm smart-mode-line mocha expand-region company-tern tern auctex-lua auctex htmlize aggressive-indent aggressive-indent-mode lacarte tidy go-eldoc edit-server skewer-mode virtualenvwrapper scss-mode rvm ruby-refactor ruby-electric ruby-compilation python-django nose go-snippets fill-column-indicator elixir-yasnippets alchemist ace-jump-mode)))
+ '(pos-tip-background-color "#073642")
+ '(pos-tip-foreground-color "#93a1a1")
 '(rcirc-bright-nicks
 (quote
  ("jwiegley" "bbatsov" "hiredman" "technomancy" "kodein" "wasamasa" "wgreenhouse")))
